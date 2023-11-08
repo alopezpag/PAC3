4069c08c2ac00169b7cc1c0974b4ca1c75458752
ArnauLP <arnaulopez004@outlook.com>
2023-11-08 23:40:10 +0100

    continuat

--- PAC3Ex1/src/main/java/edu/uoc/pac3/Passport.java
+++ PAC3Ex1/src/main/java/edu/uoc/pac3/Passport.java
@@ -1,8 +1,31 @@
 package edu.uoc.pac3;
 
+import java.time.LocalDate;
+
 public class Passport {
+    // constants
     public static final String PASSPORT_NUMBER_ERROR = "Passport number cannot be null or empty.";
-    public static final String ISSUE_DATE_ERROR = "Passport number cannot be null or empty.";
-    public static final String EXPIRATION_DATE_ERROR = "Passport number cannot be null or empty.";
-    public static final String VISA = "Passport number cannot be null or empty.";
+    public static final String ISSUE_DATE_ERROR = "Issue date must be in the last 10 years.";
+    public static final String EXPIRATION_DATE_ERROR = "Expiration date must be 10 years after the issue date.";
+    public static final String VISA_TYPE_ERROR = "Visa type must be a non-negative value.";
+
+    // variables
+    private String passportNumber;
+    private LocalDate issueDate;
+    private LocalDate expirationDate;
+    private int visaType;
+
+    // constructors
+    public Passport(String passportNumber, LocalDate issueDate, LocalDate expirationDate, int visaType) {
+        this.passportNumber = passportNumber;
+        this.issueDate = issueDate;
+        this.expirationDate = expirationDate;
+        this.visaType = visaType;
+    }
+
+    // methods
+    public String getPassportNumber() {
+        return passportNumber;
+    }
+
 }
